8cbe4d4cdcdaecf132fd0ed4052a0272d6d5e58d
Qasim Khan <Qasim@Maliks-MacBook-Pro-2.local>
2016-11-02 22:46:09 -0400

    Figured out CacheSolver for the Matrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Create two variables that store data in the parent environment. And then write two
+## functions: one that points to them and the other that assigns the value/checks if 
+## value is there. In this way we can avoid solving everytime.
 
-## Write a short comment describing this function
+## This function defines two variables and four functions to basically give us tags to 
+## refer to when setting up the cache
 
 makeCacheMatrix <- function(x = matrix()) {
-
+                sol <- NULL
+                set <- function(y) {
+                        x <<- y
+                        sol <<- NULL
+                }
+                get <- function() x
+                setsolve <- function(solve) sol <<- solve
+                getsolve <- function() sol
+                list(set = set, get = get,
+                     setsolve = setsolve,
+                     getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## This function goes into our reference variables to check if a value exists. Otherwise
+## it executes and updates the cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+                sol <- x$getsolve()
+                if(!is.null(sol)) {
+                        message("getting cached data")
+                        return(sol)
+                }
+                data <- x$get()
+                sol <- solve(data, ...)
+                x$setsolve(sol)
+                sol
 }
